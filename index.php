1df093edfcdaa734b6372e79862833d46f1d6f65
alqxyz <alqx.villalobos@gmail.com>
2019-03-16 07:06:15 -0400

    Ctrl ERA + Semantic UI

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es-VE">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no" name="viewport" />
+	<title>Semantic UI</title>
+	<link rel="shortcut icon" href="base/E/img/icon/CtrlERA.png" type="image/x-icon">
+	<link rel="stylesheet" type="text/css" href="base/E/css/lato/lato.css">
+	<link rel="stylesheet" type="text/css" href="base/E/css/semantic-UI/semantic.css">
+</head>
+<body>
+
+	<div class="ui text container">
+			<h1 class="ui dividing header">Sistema de columnas</h1>
+			<h3 class="first">Grillas</h3>
+			<p>
+				<code>Se puede utilizar hasta <b>16</b> columnas.</code>
+			</p>
+	</div>
+<hr>
+	<div class="ui container">
+		<div class="ui grid">
+			<div class="column">1</div>
+			<div class="column">2</div>
+			<div class="column">3</div>
+			<div class="column">4</div>
+			<div class="column">5</div>
+			<div class="column">6</div>
+			<div class="column">7</div>
+			<div class="column">8</div>
+			<div class="column">9</div>
+			<div class="column">10</div>
+			<div class="column">11</div>
+			<div class="column">12</div>
+			<div class="column">13</div>
+			<div class="column">14</div>
+			<div class="column">15</div>
+			<div class="column">16</div>
+		</div>
+	</div>
+
+</body>
+<script src="base/E/js/jquery/jquery-3.3.1.min.js"></script>
+<script src="base/E/js/semantic-UI/semantic.js"></script>
+</html>
